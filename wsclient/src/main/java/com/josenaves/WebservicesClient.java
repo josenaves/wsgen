1b8b3f9fc3c5a7aeb79a3ba4b5f6a623f9cad8f7
Jose Naves Moura Neto <jnaves@ig.com>
2012-09-06 16:47:33 -0300

    Novos wsdl's

--- wsclient/src/main/java/com/josenaves/WebservicesClient.java
+++ wsclient/src/main/java/com/josenaves/WebservicesClient.java
@@ -1,13 +1,17 @@
 package com.josenaves;
 
-import com.josenaves.wsclient.Calculator;
-import com.josenaves.wsclient.ICalculator;
+import net.webservicex.ConvertTemperature;
+import net.webservicex.ConvertTemperatureSoap;
+import net.webservicex.TemperatureUnit;
+
+import com.parasoft.wsdl.calculator.Calculator;
+import com.parasoft.wsdl.calculator.ICalculator;
 
 public class WebservicesClient {
 
 	public static void main(String[] args) {
 
-		System.out.println("Testa webservice Calculator...");
+		System.out.println("Testando webservice Calculator...");
 		ICalculator calcService = new Calculator().getICalculator();
 
 		System.out.println("add(1.2, 1.8)");
@@ -18,5 +22,13 @@ public class WebservicesClient {
 		resultado = calcService.subtract(3f, 1.5f);
 		System.out.println("Resultado = " + resultado);
 		
+		
+		System.out.println("-------");
+		
+		System.out.println("Testando webservice de conversão de temperatura...");
+		ConvertTemperatureSoap tempService = new ConvertTemperature().getConvertTemperatureSoap();
+		System.out.println("convertTemp(30, celsius, fahrenheit)");
+		double temp = tempService.convertTemp(30, TemperatureUnit.DEGREE_CELSIUS, TemperatureUnit.DEGREE_FAHRENHEIT);
+		System.out.println("Resultado = " + temp);
 	}
 }
